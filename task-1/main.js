ed63913757295f87b00f533fb8f56da53f7110f5
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:42:32 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,26 @@
-const prompt = require('prompt');
-prompt.start();
+// task 1
+let a;//100;//100 manat   100
+let b;//20//30 manat
+let c; //1//2 manat        2
+function sum(count){
+   let  price ;
+   if(count<1000||count>=100){
+   c=count%10+((parseInt(count/10)%10)%2)*10;// teklik
+   b=parseInt((parseInt(count/10)%10)/2)// onluq
+   a=parseInt(count/100)// yuzluk
+}
+   if(count<100){
+   c=count%10+((parseInt(count/10)%10)%2)*10;
+   b=parseInt((parseInt(count/10)%10)/2)
+   a=0
+}
+if(count<10){
+      a=0;
+      b=0;
+      c=count; }
+   price=a*100+b*30+c*2
+ console.log(price)}
+   sum(33);
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
