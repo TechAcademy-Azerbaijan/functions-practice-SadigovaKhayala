2516108ca174b134400a9269e2587ff99add079d
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:43:12 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,15 @@
-const prompt = require('prompt');
-prompt.start();
+
+//Task 2
+let a;
+function primary(a){
+let result="Yes"
+for(i=2;i<a;i++){
+   if(a%i==0){result="No"}
+   }console.log(result)}
+primary(6);
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+
+
+
