998b18e49a536f086368d1b3caeeaf0444426140
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:45:28 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
+
+task 6
+function sum(a,b){
+   let c=a*b;
+   console.log(c)
+}
+sum(2,3)
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
