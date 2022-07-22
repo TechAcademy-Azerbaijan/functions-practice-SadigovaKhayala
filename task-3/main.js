52ee06a0c53c6eec0973bb2828fdc892651d6000
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:44:03 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
+
+//TASK4
+const fibonacci = n => {
+   let a = 0, b = 1, c = n;//6
+   for(let i = 2; i <= n; i++) {
+     c = a + b;
+     a = b;
+     b = c;
+   }
+   console.log(c) ;
+ }
+ fibonacci(6)
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+
+
+
+
+
