0198f8cd8ca85088a3a8fd19fc98ceff087b2252
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:44:46 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt');
-prompt.start();
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+//task5
+function arrs (...arr){
+let temp=0;
+for(i=0;i<arr.length;i++){
+   if(arr[i]>temp){
+      temp=arr[i]
+   }
+}
+console.log(temp)}
+arrs(1,2,3,4)
