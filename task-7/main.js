9b0da28011312f016ae459231f864fd6fb296927
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-22 18:45:56 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,16 @@
-const prompt = require('prompt');
-prompt.start();
+
+//task 7
+function kv(n){
+let a;
+for(i=1;i<n;i++){
+   if(i**2<n){
+   a=i**2
+   console.log(a)}
+}}
+kv(10)
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+
+
+
